75e32b3fd51bf532ad7dde2c33fc86bda2b7a6b0
Clifford Goh <cliffordgoh25@gmail.com>
2022-09-26 17:33:43 +0800

    added shorturl generating feature

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const shortid = require('shortid');
 const dns = require('dns');
 
 
@@ -11,6 +10,9 @@ const app = express();
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('connected', function() {
+  console.log('MongoDB has connected successfully');
+});
 
 //Init body parser
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,71 +28,105 @@ app.get('/', async function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//API endpoint 
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-/** Function to validate if a host exists
- * 
- * @param {} inputURL - a URL to validate
- * returns hosturl (e.g. www.youtube.com) if host exists
- *         "" if host does not exist
- */
- function validateUrl(inputURL)  {
-  var hosturl = inputURL.toString();
-  
-  //remove protocol//
-  hosturl = hosturl.split("//")[1];
-  console.log("removed protocol in front of URL....current URL: %j", hosturl);
-
-  //remove path
-  hosturl = hosturl.split("/")[0];
-  console.log("This is the hostname %j", hosturl);
-  
-  dns.lookup(hosturl, (err, addr, family) => {
-      if(!addr) {
-          return "";
-      } else {
-          return hosturl; 
-      }
-  });
-}
-
-/*SET UP MONGOOSE MODEL */
+/*** SET UP MONGOOSE MODEL */
 const UrlSchema = new mongoose.Schema({
-  originalURL: {
+  originalUrl: {
     type: String,
-    required: [true, "enter the original URL"],
+    required: true,
   },
-  shortURL: {
+  shortUrl: {
+    type: Number,
+    required: true,
+  },
+  date: {
     type: String,
-    required: [true, "enter short url"],
+    default: Date.now,
   },
+  nextShortUrl:  {
+    type: Number,
+    required: true,
+  }
 });
 
-const Url = mongoose.model('Url', UrlSchema);
+var Url = mongoose.model('Url', UrlSchema);
+
+//adding to database  
+/* Url.create([
+  {
+    originalUrl: "https://blog.logrocket.com/building-a-url-shortener-with-node-js/", 
+    shortUrl: 0,
+    date: new Date(),
+    nextShortUrl: 1,
+  }
+]);    */
 
 
-/*POST TO /api/shorturl */
-app.post("/api/shorturl", async (req, res) => {
-  //TODO: get original url information from request body
+//POST TO /api/shorturl 
+app.post("/api/shorturl", (req, res) => {
+  let { url } = req.body;
+  console.log("Url: %j", url);
 
-  //TODO: validate original url
+  let original_url = url;
+  let short_url;
+  
+  //process URL to get hostname
+  let hostname = String(url);
+  hostname = hostname.replace("https://", "");  //remove https protocol from url
+  hostname = hostname.split("/")[0];  //removes subdomains
+  console.log("Hostname: %j", hostname);
 
-  //TODO: if valid, generate shorturl, add to database, send JSON of orignal_url and short_url
+  //validate hostname
+  dns.lookup(hostname, async (err, address) => {
+    if(err)  {  //invalid hostname
+      res.json({ error: "invalid URL" });
+    }  else {   //valid hostname
+      try {  
+        url_document = await Url.findOne({ originalUrl: url });
 
-  //TODO: else, send JSON of error
-});
+        if(url_document != null)  { //shortURL already generated previously
+          short_url = url_document.shortUrl;
+          console.log("Short URL: %j", short_url);
 
-/*POST TO /api/shorturl/:code */
+          res.json({
+            original_url: original_url,
+            short_url: short_url,
+          });
+
+        } else {  //no shortUrl generated yet
+          Url.find({}).sort({ date: -1 }).limit(1).exec((err, recentUrl) => {
+            short_url = recentUrl[0].nextShortUrl;  //generate shortURL from previous shortURL
+            
+            Url.create({
+              originalUrl: url,
+              shortUrl: short_url,
+              date: new Date(),
+              nextShortUrl: short_url + 1,
+            });
+
+            console.log("Created new shortURL: %j", short_url);
+
+            res.json({
+              original_url: original_url,
+              short_url: short_url,
+            });
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+}); 
+
+
+/*** POST TO /api/shorturl/:code */
 app.post("api/shorturl/:code", (req, res) => {
-  //TODO: get code from params
-
-  //TODO: search in database 
-
-  //TODO: get original url
-
-  //TODO: redirect to original url
+  
 });
 
 
